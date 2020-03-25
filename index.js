59377ce9320dfb3aab7ea0427a854d4980576715
Marcin Rojek <marcin.rojek@outlook.com>
2020-03-25 18:06:34 +0100

    app is checking if point is inside square

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ selectElement.addEventListener("change", event => {
 });
 
 ctx.fillRect(0, 0, canvas.getAttribute("height"), canvas.getAttribute("width"));
-
-//report the mouse position on click
+var r = new MyRect(100, 30, 80, 80);
+//drawing point on mouse click
 canvas.addEventListener(
   "click",
   function(evt) {
@@ -23,9 +23,19 @@ canvas.addEventListener(
     ctx.beginPath();
     ctx.arc(mousePos.x, mousePos.y, 2, 0, Math.PI * 2, true);
     ctx.fill();
+
+    if (r.contains(mousePos.x, mousePos.y)) {
+      //alert("punkt");
+      document.getElementById("result").innerHTML = inside;
+    } else {
+      document.getElementById("result").innerHTML = outside;
+    }
   },
   false
 );
+
+var inside = '<div class="alert alert-primary" role="alert">Punkt jest wewnątrz</div>;';
+var outside = '<div class="alert alert-danger" role="alert">Punkt jest na zewnątrz</div >';
 
 //Get Mouse Position
 function getMousePos(canvas, evt) {
@@ -53,7 +63,8 @@ function drawFigure(x) {
     var ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    ctx.strokeRect(100, 30, 80, 80);
+    r.draw(ctx);
+    //ctx.strokeRect(100, 30, 80, 80);
   }
   if (x == "prostokąt") {
     var canvas = document.getElementById("myCanvas");
@@ -77,4 +88,18 @@ function drawFigure(x) {
 
     ctx.stroke();
   }
+}
+function MyRect(x, y, w, h) {
+  this.x = x;
+  this.y = y;
+  this.width = w;
+  this.height = h;
+
+  this.contains = function(x, y) {
+    return this.x <= x && x <= this.x + this.width && this.y <= y && y <= this.y + this.height;
+  };
+
+  this.draw = function(ctx) {
+    ctx.strokeRect(this.x, this.y, this.width, this.height);
+  };
 }
