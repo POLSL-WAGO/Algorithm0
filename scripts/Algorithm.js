85f51125c59e01b3de26d3a8f73ee8594a8370be
Marcin Rojek <marcin.rojek@outlook.com>
2020-04-03 17:37:17 +0200

    jarvis algorithm added

--- scripts/Algorithm.js
+++ scripts/Algorithm.js
@@ -16,3 +16,28 @@ function draw(e) {
   context.fillStyle = "#000000";
   context.fillRect(posx, posy, 4, 4);
 }
+///////////////////////////////////////////////////
+
+var canvas = document.getElementById("gameCanvas");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "rgba(1,1,1,1)";
+ctx.fillRect(0, 0, canvas.getAttribute("height"), canvas.getAttribute("width"));
+
+//report the mouse position on click
+canvas.addEventListener(
+  "click",
+  function(evt) {
+    var mousePos = getMousePos(canvas, evt);
+    alert(mousePos.x + "," + mousePos.y);
+  },
+  false
+);
+
+//Get Mouse Position
+function getMousePos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top
+  };
+}
